bc0aec61f07288dbe330dd62ee892ba88ccd78cf
Shalabh singh yadav <95340404+Shalabhsinghyadav01@users.noreply.github.com>
2026-04-21 20:21:56 -0400

    Add: anagram-groups - submission-3

---
+++ Data Structures & Algorithms/anagram-groups/submission-3.py
@@ -0,0 +1,12 @@
+from collections import Counter
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        hash_map = {}
+        for s in strs:
+            key =  tuple(sorted(Counter(s).items()))
+            if key in hash_map:
+                hash_map[key].append(s)
+            else:
+                hash_map[key] = [s]
+        return list(hash_map.values())
